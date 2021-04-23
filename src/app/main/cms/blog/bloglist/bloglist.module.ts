7686964fb81d8081ba1f5136cc1e3f201bdd784b
NguyenTuan-12 <tuannguyen01214@gmail.com>
2021-04-23 17:01:47 +0700

    final2304

--- src/app/main/cms/blog/bloglist/bloglist.module.ts
+++ src/app/main/cms/blog/bloglist/bloglist.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlogListComponent } from './bloglist.component';
+import { BlogListEditComponent } from './edit/bloglist-edit.component';
 import { TlaSharedModule } from 'src/app/components/shared.module';
 import { CoreService } from 'src/app/_services/core.service';
-
+import { ToolbarService, LinkService, ImageService, HtmlEditorService } from '@syncfusion/ej2-angular-richtexteditor';
+import { ImgurApiService } from 'src/app/_services/imgur.service';
+// toolbar + imgur (2 import tren se hien list-edit-component)
+// dong thoi khai bao trong provider: 
 
 const routes: Routes = [
   {
     path: '',
     component: BlogListComponent
   },
+  {
+    path: ':id',
+    component: BlogListEditComponent
+  }
 ];
 
 @NgModule({
@@ -17,7 +25,7 @@ const routes: Routes = [
     RouterModule.forChild(routes),
     TlaSharedModule
   ],
-  declarations: [BlogListComponent],
-  providers: [CoreService]
+  declarations: [BlogListComponent,BlogListEditComponent],
+  providers: [CoreService,ToolbarService, LinkService, ImageService, HtmlEditorService,ImgurApiService]
 })
 export class BlogListModule {}
